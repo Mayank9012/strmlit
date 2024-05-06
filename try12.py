420ae6d3ce8efb545d0b0d7609a85141eb1ff288
Mayank9012 <166604439+Mayank9012@users.noreply.github.com>
2024-05-06 17:10:20 +0530

    Update try12.py

--- try12.py
+++ try12.py
@@ -2,17 +2,18 @@ import streamlit as st
 
 st.title("""Streamlit demo""")
 st.write("Details")
-option = ['A','B','C','D','']
+Gender = ['Male','Female','Others']
 
 def main():
   with st.form("Enter details:"):
     name = st.text_input("Enter Name: ",)
-    st.selectbox("Option:", options=option)
+    gen = st.selectbox("Option:", options=option)
     Age = st.slider("Age",1,99,value=0,format="%d")
     submit = st.form_submit_button("Submit")
 
   if submit:
     st.write("Name : ",name)
+    st.write("Gender : ",gen)
     st.write("Group : ")
     if Age<18:
       st.write("K")
