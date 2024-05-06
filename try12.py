e26ed28104d91987f74ff68bdcfb3320fb4c2e77
Mayank9012 <166604439+Mayank9012@users.noreply.github.com>
2024-05-06 16:43:42 +0530

    Update try12.py

--- try12.py
+++ try12.py
@@ -7,6 +7,6 @@ first
 option = ['A','B','C','D','']
 
 def main():
-  with st.form("Enter details:")
-  st.selectbox("Option:", options=option)
-  st.slider("Age",10,99,value=0,format="%d")
+  with st.form("Enter details:"):
+    st.selectbox("Option:", options=option)
+    st.slider("Age",10,99,value=0,format="%d")
