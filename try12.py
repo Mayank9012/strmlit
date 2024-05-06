c5854eb5782775c8218766063e8b8ebb94ab7557
Mayank9012 <166604439+Mayank9012@users.noreply.github.com>
2024-05-06 16:59:03 +0530

    Update try12.py

--- try12.py
+++ try12.py
@@ -8,13 +8,13 @@ def main():
   with st.form("Enter details:"):
     st.text_input("Enter Name: ")
     st.selectbox("Option:", options=option)
-    st.slider("Age",1,99,value=0,format="%d")
+    Age = st.slider("Age",1,99,value=0,format="%d")
     submit = st.form_submit_button("Submit")
 
   if submit:
-    if (Age<18):
+    if Age<18:
       print("K")
-    elif (Age>=18 and Age<30):
+    elif Age>=18 and Age=<30:
       print("A")
     else:
       print("O")
