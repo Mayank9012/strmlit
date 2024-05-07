314f815b958a7abcb862bb0680a91c383cb25a6f
Mayank9012 <166604439+Mayank9012@users.noreply.github.com>
2024-05-07 11:44:39 +0530

    Update try12.py

--- try12.py
+++ try12.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit_folium as st_folium
 
 css="""<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">"""
 st.markdown(css,unsaf_allow_html=True)
