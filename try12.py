05efa13bf97eae2ebd4262b509d4a716a7810e12
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-06-15 15:58:27 +0530

    Update try12.py

--- try12.py
+++ try12.py
@@ -452,7 +452,7 @@ st.markdown("""
                 letter-spacing: 4px;
                 overflow: hidden;
                 color:#1c3b29;       
-                transform: translateY(-8vw) translateX(5vh);
+                transform: translateY(-13vw) translateX(4.5vh);
 		    }
 
 		.logo {
