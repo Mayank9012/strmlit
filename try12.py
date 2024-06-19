109b95e34e0d60975826ab397ed9e38bbcc404aa
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-06-19 17:07:26 +0530

    Update try12.py

--- try12.py
+++ try12.py
@@ -623,7 +623,7 @@ st.markdown("""
 
             [data-testid="baseButton-secondary"]{
                 display:inline-block;
-                transform: translateX(28%) translateY(-703%);
+                transform: translateX(28%) translateY(-723%);
 				width: 135px;
 				height:35px;
 				border: 2px solid #3d6154;
